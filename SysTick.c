aac901cb62afd0d169ba0205ff3f8e4d32602bbd
Unknown <omarko.kamel@gmail.com>
2018-06-30 18:46:27 +0200

    ADDIng project files

---
+++ SysTick.c
@@ -0,0 +1,38 @@
+// SysTick.h
+// Runs on LM4F120
+// Provide functions that initialize the SysTick module, wait at least a
+// designated number of clock cycles, and wait approximately a multiple
+// of 10 milliseconds using busy wait.  After a power-on-reset, the
+// LM4F120 gets its clock from the 16 MHz precision internal oscillator,
+// which can vary by +/- 1% at room temperature and +/- 3% across all
+// temperature ranges.  If you are using this module, you may need more
+// precise timing, so it is assumed that you are using the PLL to set
+// the system clock to 50 MHz.  This matters for the function
+// SysTick_Wait10ms(), which will wait longer than 10 ms if the clock is
+// slower.
+
+
+
+// Initialize SysTick with busy wait running at bus clock.
+#define NVIC_ST_CTRL_R      (*((volatile unsigned long *)0xE000E010))
+#define NVIC_ST_RELOAD_R    (*((volatile unsigned long *)0xE000E014))
+#define NVIC_ST_CURRENT_R   (*((volatile unsigned long *)0xE000E018))
+void SysTick_Init(void){
+  NVIC_ST_CTRL_R = 0;               // disable SysTick during setup
+  NVIC_ST_CTRL_R = 0x00000005;      // enable SysTick with core clock
+}
+// The delay parameter is in units of the 80 MHz core clock. (12.5 ns)
+void SysTick_Wait(unsigned long delay){
+  NVIC_ST_RELOAD_R = delay-1;  // number of counts to wait
+  NVIC_ST_CURRENT_R = 0;       // any value written to CURRENT clears
+  while((NVIC_ST_CTRL_R&0x00010000)==0){ // wait for count flag
+  }
+}
+// 10000us equals 10ms
+void SysTick_Wait10ms(unsigned long delay){
+  unsigned long i;
+  for(i=0; i<delay; i++){
+    SysTick_Wait(800000);  // wait 10ms
+  }
+}
+
